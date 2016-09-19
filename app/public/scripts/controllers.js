e9c05840038cf56de0e83bfb1308270e1c28bdaa
Roy <rechase84@gmail.com>
2016-09-19 17:27:38 -0600

    sort of working donate view

--- app/public/scripts/controllers.js
+++ app/public/scripts/controllers.js
@@ -33,8 +33,21 @@ app.controller('signupController', ['SignUpService', function(SignUpService){
   };
 }]);
 
-app.controller('donateController', ['$location','$window','LogoutService', function($location,$window, LogoutService){
+app.controller('donateController', ['$location','$window','LogoutService', function($location, $window, LogoutService){
   var vm = this;
+  vm.form = {};
+  vm.currentDate = Date.now();
+  console.log(vm.currentDate);
+  vm.maxFutureDate = new Date();
+  vm.maxFutureDate.setDate(vm.maxFutureDate.getDate() + 30);
+  console.log(vm.maxFutureDate);
+  vm.futureDate = $window.setFutureDate;
+  console.log(vm.futureDate);
+  // vm.setFutureDate = function($window){
+  //   futureDate = $window.setNewDate;
+  // }
+  // vm.getCurrentDateTime = vm.currentDate.setDate();
+  // vm.setMaxFutureDate = vm.futureDate.setDate(30);
   vm.logOut = LogoutService.logOut;
   //if user is not logged in we want them to be so this redirs to login
   if(!$window.localStorage.token){
