f20f7464069119dae5c914765ec33a6a17f04d9f
Roy <rechase84@gmail.com>
2016-09-27 16:24:47 -0600

    ..

--- app/public/scripts/services.js
+++ app/public/scripts/services.js
@@ -91,7 +91,7 @@ app.service('DonateService', ['$window', '$http', function($window, $http){
 	sv.getClientToken = function(){
 		// var jwt = $window.localStorage.token;
 		$http.get("//mysterious-dusk-96055.herokuapp.com/getToken",{
-			user_id : $window.localStorage.id
+			// user_id : $window.localStorage.id
 		})
 		.then(function(response){
 			$window.localStorage.clientToken = response.data.clientToken;
