20cd714cf009b9d89b0ecd354a1f5609aaa7d5eb
Roy <rechase84@gmail.com>
2016-09-27 18:40:33 -0600

    out of useful messages:

--- app/public/scripts/braintree.js
+++ app/public/scripts/braintree.js
@@ -50,7 +50,7 @@ braintree.client.create({
     // (function(){
     //   $.ajax({
     //     method: "GET",
-    //     url: "https://mysterious-dusk-96055.herokuapp.com/getToken",
+    //     url: "//mysterious-dusk-96055.herokuapp.com/getToken",
     //     data: {user_id : window.localStorage.id}
     //   })
     //   .done(function(response){
@@ -79,7 +79,7 @@ braintree.client.create({
 				// console.log(amountFromClient);
 				$.ajax({
 					method: "POST",
-					url: "https://mysterious-dusk-96055.herokuapp.com/processPayment",
+					url: "//mysterious-dusk-96055.herokuapp.com/processPayment",
 					data: {payload: payload.nonce,
 								amount: amountFromClient}
 				})
